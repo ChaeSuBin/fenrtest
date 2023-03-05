468e4664b2843bbdb2bb5e8e5c3651e99eacf4a1
GOGACOLA\chae6 <k96765435@gmail.com>
2023-03-05 19:52:54 +0900

    modi api uri

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
-async function request(path, options = {}) {
-    const secret = process.env.REACT_APP_HOTPEPPER_API_KEY;
-    const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${secret}&format=json${path}`;
-    const response = await fetch(url, options);
-    return response.json();
-}
-
 // async function request(path, options = {}) {
 //     const secret = process.env.REACT_APP_HOTPEPPER_API_KEY;
-//     const url = `/gourmet/v1/?key=${secret}&format=json${path}`;
+//     const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${secret}&format=json${path}`;
 //     const response = await fetch(url, options);
 //     return response.json();
 // }
+
+async function request(path, options = {}) {
+    const secret = process.env.REACT_APP_HOTPEPPER_API_KEY;
+    const url = `/gourmet/v1/?key=${secret}&format=json${path}`;
+    const response = await fetch(url, options);
+    return response.json();
+}
 
 export async function getStoreList(_searchFrom, _searchCount, _lat, _lng){
     return request(`&lat=${_lat}&lng=${_lng}&range=${_searchCount[1]}&order=1&start=${_searchFrom}&count=${_searchCount[0]}`);
