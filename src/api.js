3bea672b9f3f4b937d7630b9bc894769cfbc7d15
GOGACOLA\chae6 <k96765435@gmail.com>
2023-03-05 16:09:43 +0900

    debug mode

--- src/api.js
+++ src/api.js
@@ -2,8 +2,6 @@ async function request(path, options = {}) {
     const secret = process.env.REACT_APP_HOTPEPPER_API_KEY;
     const url = `/gourmet/v1/?key=${secret}&format=json${path}`;
     const response = await fetch(url, options);
-    console.log('k', secret)
-    console.log('v', response)
     return response.json();
 }
 
@@ -11,7 +9,6 @@ async function request(path, options = {}) {
 //     return request(`&large_area=Z011&start=${_pageNum}`, {mode: 'cors', credentials: 'include'});
 // }
 export async function getStoreList(_searchFrom, _searchCount, _lat, _lng){
-    console.log('dev')
     return request(`&lat=${_lat}&lng=${_lng}&range=${_searchCount[1]}&order=1&start=${_searchFrom}&count=${_searchCount[0]}`);
 }
 export async function getStoreInfo(_storeId){
